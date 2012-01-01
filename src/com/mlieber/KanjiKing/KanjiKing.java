644e02602d793cd5fe16f2ab56bd25214a19b99d
Mathias Lieber <mathias@familie-lieber.de>
2012-01-01 16:32:15 +0100

    Started KanjiKing development

---
+++ src/com/mlieber/KanjiKing/KanjiKing.java
@@ -0,0 +1,15 @@
+package com.mlieber.KanjiKing;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class KanjiKing extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
