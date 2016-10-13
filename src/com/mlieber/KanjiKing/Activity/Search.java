5187ca37dcd5b82b4af21a5ee27a16f43e10a168
Mathias Lieber <lieber@bitnetic.de>
2016-10-13 23:07:19 +0200

    KKA-25: Search: Show radical table

--- src/com/mlieber/KanjiKing/Activity/Search.java
+++ src/com/mlieber/KanjiKing/Activity/Search.java
@@ -1,13 +1,14 @@
 package com.mlieber.KanjiKing.Activity;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.EditText;
-import android.widget.SeekBar;
+import android.view.ViewGroup;
+import android.widget.*;
 import android.util.Log;
+
 import com.mlieber.KanjiKing.*;
 import com.mlieber.KanjiKing.CardBox.Card;
 import com.mlieber.KanjiKing.CardBox.CardBox;
@@ -23,12 +24,16 @@ public class Search extends Activity
 {
     private static final String TAG = "KanjiKing/Search";
 
+    private LinearLayout _search_form;
+    private LinearLayout _search_result_area;
+
     private EditText _search_phrase;
     private EditText _search_reading;
     private EditText _search_meaning;
     private SeekBar _search_radical;
     private SeekBar _search_strokes;
     private Button _search_button;
+    private Button _search_edit_button;
     private TextView _search_result;
     private TextView _search_radical_preview;
     private TextView _search_strokes_preview;
@@ -49,6 +54,11 @@ public class Search extends Activity
         // use the search view here
         setContentView(R.layout.search);
 
+        // Areas
+        _search_form = (LinearLayout) findViewById(R.id.search_form);
+        _search_result_area = (LinearLayout) findViewById(R.id.search_result_area);
+
+        // Elements
         _search_phrase = (EditText) findViewById(R.id.search_word);
         _search_reading = (EditText) findViewById(R.id.search_reading);
         _search_meaning = (EditText) findViewById(R.id.search_meaning);
@@ -58,7 +68,8 @@ public class Search extends Activity
         _search_strokes_preview = (TextView) findViewById(R.id.abcde123);
         _search_button = (Button)   findViewById(R.id.search_button);
         _search_result = (TextView) findViewById(R.id.search_result);
-  
+        _search_edit_button = (Button) findViewById(R.id.search_edit_button);
+
         // animate strokes slider
         _search_strokes.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             public void onProgressChanged(SeekBar b, int progress, boolean fromUser) {
@@ -93,9 +104,32 @@ public class Search extends Activity
             public void onStopTrackingTouch(SeekBar b) { }
         });
 
+        // Create radical button grid - programmatically
+        TableLayout radicalTable = (TableLayout) findViewById(R.id.search_radical_grid);
+        for (int row = 0; row < 22; row++) {
+            TableRow newRow = new TableRow(this);
+            newRow.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+
+            for (int c = 0; c < 10; c++) {
+                int sum = (10 * row + c);
+                if (sum >= 214) break;
+                Button radicalButton = new Button(this);
+                radicalButton.setText(_cardstore.get(((sum + 1) + 3000) + "").getOnReading());
+                radicalButton.setBackgroundResource(R.drawable.radical);
+                radicalButton.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 10.0f));
+                newRow.addView(radicalButton);
+            }
+            radicalTable.addView(newRow, new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
+        }
+
         // connect search button
         _search_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+
+                _search_form.setVisibility(View.GONE);
+                _search_result.setText("Searching...");
+                _search_result_area.setVisibility(View.VISIBLE);
+
                 Criteria criteria = new Criteria();
                 criteria.setSearchPhrase((((TextView) _search_phrase).getText()).toString())
                         .setReading((((TextView) _search_reading).getText()).toString())
@@ -103,15 +137,24 @@ public class Search extends Activity
                         .setRadical(_search_radical.getProgress())
                         .setStrokes(_search_strokes.getProgress());
 
-                _search_result.setText("Searching...");
-
                 Card[] result = search(criteria);
 
                 _search_result.setText(
                     formatSearchResult(result)
                 );
+
             }
         });
+
+        _search_edit_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                _search_result_area.setVisibility(View.GONE);
+                _search_form.setVisibility(View.VISIBLE);
+            }
+        });
+
+        _search_result_area.setVisibility(View.GONE);
+        _search_form.setVisibility(View.VISIBLE);
     }
 
     private Card[] search(Criteria criteria) {
@@ -136,4 +179,3 @@ public class Search extends Activity
         return sb.toString();
     }
 }
-
